952e2f2e84f43f3ae7e0d204365a231b14e4d7ed
spokrovskiy <spokrovskey@gmail.com>
2018-02-27 22:07:47 -0700

    finished setting up environment

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var browserify = require('browserify'); // Bundles JS
 var reactify = require('reactify');  // Transforms React JSX to JS
 var source = require('vinyl-source-stream'); // Use conventional text streams with Gulp
 var concat = require('gulp-concat'); //Concatenates files
-var lint = require('gulp-eslint'); //Lint JS files, including JSX
+// var lint = require('gulp-eslint'); //Lint JS files, including JSX
 
 var config = {
 	port: 9005,
@@ -61,15 +61,15 @@ gulp.task('css', function() {
 		.pipe(gulp.dest(config.paths.dist + '/css'));
 });
 
-gulp.task('lint', function() {
-	return gulp.src(config.paths.js)
-		.pipe(lint({config: 'eslint.config.json'}))
-		.pipe(lint.format());
-});
+// gulp.task('lint', function() {
+// 	return gulp.src(config.paths.js)
+// 		.pipe(lint({config: 'eslint.config.json'}))
+// 		.pipe(lint.format());
+// });
 
 gulp.task('watch', function() {
 	gulp.watch(config.paths.html, ['html']);
 	gulp.watch(config.paths.js, ['js', 'lint']);
 });
 
-gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']);
+gulp.task('default', ['html', 'js', 'css', 'open', 'watch']);
